917dc0137014da9d7be2b06d2534c2a5b51826b0
floglor <floglor033@gmail.com>
2019-12-22 16:45:49 +0200

    Oi oops, gson import.

--- lib/models/database_helper.dart
+++ lib/models/database_helper.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:comment_app/models/image_post.dart';
-import 'package:gson/gson.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
